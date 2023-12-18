f018cd4b195cdbc06779ca7689f5d0708b8e5678
Justin Bansal <justin.bansal@pixelunion.net>
2023-12-18 09:25:21 -0500

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,63 +1,57 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-  const result = parts.map(part => <Part part={part} key={part.id}/>);
-  return (
-    <>
-      {result}
-    </>
-  )
-}
+import Course from './Course';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 10,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const Course = ({ course }) => {
-    const total = course.parts.reduce((sum, part) => {
-      return sum + part.exercises;
-    }, 0);
-    return (
-      <>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total sum={total}/>
-      </>
-    )
-  }
-
-  return <Course course={course} />
+  const result = courses.map(course => <Course course={course} key={course.id} />);
+  return (
+    <div>
+      {result}
+    </div>
+  )
 }
 
 export default App
