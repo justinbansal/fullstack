b024f971349dbaa359626ad7843d1b903a600638
Justin Bansal <justin.bansal@pixelunion.net>
2023-12-18 12:13:44 -0500

    phonebook 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    }
+    const personsCopy = persons.concat(newPerson);
+    setPersons(personsCopy);
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App;
