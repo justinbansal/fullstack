33dc195b6250c3c19151118579c5087bff418365
Justin Bansal <justin.bansal@pixelunion.net>
2023-12-20 09:01:24 -0500

    adding styles to app

--- part2/app/src/App.jsx
+++ part2/app/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import noteService from './services/notes';
 import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('some error happened...');
 
   const hook = () => {
     noteService
@@ -43,7 +46,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote));
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `the note '${note.content}' was already deleted from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id));
       })
   }
@@ -59,6 +67,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all' }</button>
       </div>
@@ -71,6 +80,7 @@ const App = () => {
         <input type="text" value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
